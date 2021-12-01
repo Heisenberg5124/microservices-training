d76add9d140d3caaa406e6376e1f362124ee6589
Heisenberg5124 <dhvinhpct@gmail.com>
2021-12-01 15:19:26 +0700

    Updates

--- ticketing/common/src/events/payment-created-event.ts
+++ ticketing/common/src/events/payment-created-event.ts
@@ -4,7 +4,6 @@ export interface PaymentCreatedEvent {
     subject: Subjects.PaymentCreated;
     data: {
         id: string;
-        version: number;
         orderId: string;
         stripeId: string;
     }
